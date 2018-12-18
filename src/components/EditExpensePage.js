20de8ae1b7945c211b091777c426a0a28c99fbdb
Ian KIM <magictical@gmail.com>
2018-12-18 23:34:28 +0900

    lec80-81 use dynamic route and get data from parent and use it by props

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is EditExpensePage compnent
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            This is EditExpensePage compnent. This access Id is {props.match.params.id}
+        </div>
+    )
+};
 
 export default EditExpensePage;

f93e064d33ee5e50c62ccb5805cb5a74eb4a9b9f
Ian KIM <magictical@gmail.com>
2018-12-19 02:38:04 +0900

    lec 87 manipulates count by usiing dispatch()
    check how to subscribe & unsubscribe of the data chaning and keyword 'type' in dispatch and decrementBy, incrementBy, counter  which change number to 101

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,19 @@ import { createStore } from 'redux';
 const store = createStore((state = {count:0 }, action) => {
     switch(action.type) {
         case 'INCREMENT' :
+            //action.increment가 숫자면 그걸 incrementBy로 쓰고 아닐경우 ':' default로 1을 씀
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT' :
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1;
             return {
-                count: state.count -1
+                count: state.count - decrementBy
+            }
+        case 'SET' :
+            return {
+                count: action.counter
             }
         case 'RESET' :
             return {
@@ -20,34 +27,45 @@ const store = createStore((state = {count:0 }, action) => {
     }
 });
 
-console.log(store.getState());
 
 
 //important keyword : action
 
+// check store's state
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
 // action for increment
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+
+});
+
+// // to ubsubscribe use .subscribe() again
+// unsubscribe();
+
 store.dispatch({
     type: 'INCREMENT'
 });
-
-// action for decrement
-store.dispatch({
-    type: 'DECREMENT'
-});
-
-store.dispatch({
-    type: 'DECREMENT'
-});
-
 
 // action for reset
 store.dispatch({
     type: 'RESET'
 });
 
-
+// action for decrement
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
 
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
 });
-console.log(store.getState());
+
+store.dispatch({
+    type: 'SET',
+    counter: 101
+})

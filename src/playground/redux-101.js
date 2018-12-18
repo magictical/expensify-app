28243b3e6ce7c7fa50faa08f01cb86ca64475532
Ian KIM <magictical@gmail.com>
2018-12-19 01:49:05 +0900

    lec 86 - dispatching 'action' for redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,53 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count:0 }) => {
-    return state;
+const store = createStore((state = {count:0 }, action) => {
+    switch(action.type) {
+        case 'INCREMENT' :
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT' :
+            return {
+                count: state.count -1
+            }
+        case 'RESET' :
+            return {
+                count: state.count = 0
+            }
+        
+        default : 
+            return state;
+    }
 });
 
+console.log(store.getState());
+
+
+//important keyword : action
+
+// action for increment
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+// action for decrement
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+// action for reset
+store.dispatch({
+    type: 'RESET'
+});
+
+
+
+store.dispatch({
+    type: 'DECREMENT'
+});
 console.log(store.getState());

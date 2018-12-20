8af9cdaeba45bc374e8580a7e31d8282fe8a8b53
Ian KIM <magictical@gmail.com>
2018-12-21 00:54:14 +0900

    lec 93 - add generator, reducer for ADD_EXPENSE, REMOVE_EXPENSE

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,19 +1,54 @@
 // combineReducers - 일종의 Reducer 트래커인듯?
 import { createStore, combineReducers } from 'redux';
-import uuif from 'uuid';
+import uuid from 'uuid';
+
+
+
 
 // ADD_EXPENSE
-const addExpense = () => ({
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    }
+) => ({
     type: 'ADD_EXPENSE',
     expense: {
-        id: uuid()
-    }
-})
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    },
+});
+
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
+
 // Expenses Reducer
 const expenseReducerDefaultState = [];
 // Reducer for expenses
 const expensesReducer = (state = expenseReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                //spread operator 사용 ...은 이전의 array를 불러오는것
+                // 즉 이전 state array, action.expense를 이어서 붙이는것(삽입) 
+                ...state, 
+                action.expense
+            ]
+        //filter의 파라메터 id는 {}를 사용해서 desructuring 됨 즉 state의 천번째 
+        //property인 id를 직적 지정한거임 이걸 action.id와 비교해서 참이면 통과
+        //거짓이면 필터링 즉 state필터의 파라메터와 입력된 id값이 동일하면 filter의 
+        //새로운 array에서 제외된다.
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id )
+
         default:
             return state;
     }
@@ -44,8 +79,17 @@ const store = createStore(
 );
 
 
+// ADD subcribe
+store.subscribe(() => {
+    console.log(store.getState())
+});
 
-console.log(store.getState());
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'coffee', amount: 3 }));
+
+//remove coffee expense!
+store.dispatch(removeExpense({ id: expenseOne.expense.id}));
+
 
 
 const demoState = {

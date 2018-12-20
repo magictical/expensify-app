a18bfadb1fa26ad0bdadfc4b9948d9cd3a703a31
Ian KIM <magictical@gmail.com>
2018-12-20 15:18:18 +0900

    lec 92 - reducer with combine reducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,6 +1,46 @@
 // combineReducers - 일종의 Reducer 트래커인듯?
 import { createStore, combineReducers } from 'redux';
 
+
+// Expenses Reducer
+const expenseReducerDefaultState = [];
+// Reducer for expenses
+const expensesReducer = (state = expenseReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filter Reducer
+// Filter Reducer default Array
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filterReducers = (state = filterReducerDefaultState, action) => {
+    switch (action.type) {
+        default :
+            return state;
+    }
+}
+
+// Store creattion
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filterReducers
+    })
+);
+
+
+
+console.log(store.getState());
+
+
 const demoState = {
     expenses: [{
         id: 'askldaks',
@@ -8,5 +48,11 @@ const demoState = {
         note: 'This was the final payment for that address',
         amount: 54500,
         createdAt: 0
-    }]
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', //date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
 }

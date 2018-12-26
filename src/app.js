c285cc29acb5ddcb846c21ca7df1ba66e8904024
Ian KIM <magictical@gmail.com>
2018-12-26 14:10:43 +0900

    Section 11 lec 99 - import configure.js to app.js
    check the result on console

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+
+import configureStore from './store/configureStore';
 // node_module 에서 normalize.css import
 //webpack에서 scss 설정을 css에서 적용되도록 중간에 '?' 추가 해줘야 함
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-
+const store = configureStore();
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));

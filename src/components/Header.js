2dee8ef225a39d8664e161a0eaa0357f0c9295a4
Ian KIM <magictical@gmail.com>
2018-12-27 11:51:32 +0900

    fix title name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <ul>
-            <li><NavLink to="/" activeClassName="is-active" exact={true}>move to Home </NavLink></li>
-            <li><NavLink to="/create" activeClassName="is-active">move to create </NavLink></li>
-            <li><NavLink to="/edit" activeClassName="is-active">move to edit </NavLink></li>
-            <li><NavLink to="/help" activeClassName="is-active">move to help </NavLink></li>
+            <li><NavLink to="/" activeClassName="is-active" exact={true}>DashBoard </NavLink></li>
+            <li><NavLink to="/create" activeClassName="is-active">Create </NavLink></li>
+            <li><NavLink to="/edit" activeClassName="is-active">Expense </NavLink></li>
+            <li><NavLink to="/help" activeClassName="is-active">Help </NavLink></li>
         </ul>
     </header>
 );

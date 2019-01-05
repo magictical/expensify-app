172e2e3f109ccca7e7f86d3f73253244e1d00d8d
Ian KIM <magictical@gmail.com>
2019-01-05 11:07:45 +0900

    lec 103 - add ExpenseListFitler.js

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
         <p>This is from my dashboard component</p>
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );

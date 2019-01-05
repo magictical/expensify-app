7eb8a71bea48cb6db557edfe044e0875cfaa1bd6
Ian KIM <magictical@gmail.com>
2019-01-05 14:29:49 +0900

    lec 103 - add button for remove expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { removeExpense } from '../actions/expenses'
 
 // Export a stateless fuctional component
 // destructuring으로 props을 - Descruption, amount, createdAt 으로 
 // 지정해서 받아서 사용!
 
-const ExpenseItemList = ({ description, amount, createdAt}) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <h3>{description}</h3>
-        <p></p>{amount} - {createdAt}
+        <p>{amount} - {createdAt}</p>
+        
+        <button onClick={() =>
+            // dispatch 사용은 문법이라고 생각하자
+            dispatch(removeExpense({ id }))
+        }>Remove</button>
+        {console.log()}
     </div>
 )
+
     
 
-export default ExpenseItemList;
+export default connect()(ExpenseListItem);

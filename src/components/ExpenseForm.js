ce56a90feb6cd5120067a9b1d5ca2189162e93b1
Ian KIM <magictical@gmail.com>
2019-01-06 23:02:23 +0900

    lec 106 - setting up datepicker and tracking the date user chosen on the page

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
   //local state for tracking the data in this page
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false
   }
   // tracking the input data from user in description <input>
   onDescriptionChange = (e) => {
@@ -22,6 +31,12 @@ export default class ExpenseForm extends React.Component {
     if(amount.match(/^\d*(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }))
     }
+  }
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  }
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
   }
 
 
@@ -40,6 +55,14 @@ export default class ExpenseForm extends React.Component {
                     placeholder="Amount"
                     value={this.state.amount}
                     onChange={ this.onAmountChange }
+                  />
+                  <SingleDatePicker 
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
                   />
                   <textarea
                     placeholder="Add a note for your expense(optional)"

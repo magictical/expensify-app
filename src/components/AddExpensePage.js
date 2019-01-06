85c9912f73118a91802275b1ec7fb3a77aa63ede
Ian KIM <magictical@gmail.com>
2019-01-07 01:25:54 +0900

    lec107 - connect expense data to store
    recieve expense data from ExpenseForm and connect it to store for using them other components

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
-const AddExpensePage = () => (
+import { connect } from 'react-redux';
+import { addExpense } from '../actions/expenses';
+
+const AddExpensePage = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm 
-          onSubmit={(ace) => {
-						console.log(ace);
-						console.log("this is return value")
+				<ExpenseForm 
+					//expense 빈박스를 보내서 ExpenseFrom에서 가공한후
+					//dispatch로 store와 연결하면 이제 다른 component에도 사용가능!
+          onSubmit={(expense) => {
+						props.dispatch(addExpense(expense));
+						//move to dashboard
+						props.history.push('/');
+						
 					}}
         />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);

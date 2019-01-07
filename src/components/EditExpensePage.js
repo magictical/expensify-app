2c0bd5660b2240e63d9126b4cee3e95bc3d7f8a6
Ian KIM <magictical@gmail.com>
2019-01-08 02:09:33 +0900

    lec108 - connect editExpense to store

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const EditExpensePage = (props) => {
     console.log(props);
     return (
         <div>
-            This is EditExpensePage compnent. This access Id is {props.match.params.id}
+            This is EditExpensePage compnent. 
+            This access Id is {props.match.params.id}
         </div>
     )
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    // expenses array에서 현재 페이지의 expense id와 같은 id의 값을 찾아서 리턴
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);

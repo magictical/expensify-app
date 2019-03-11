96444313ab7e323d4cf0a83b57a6a374b04c943e
Ian KIM <magictical@gmail.com>
2019-03-11 12:01:49 +0900

    cross check test for setting up enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-// react-test-renderer
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
   // console.log('show header render test result \n', renderer.getRenderOutput());
 });

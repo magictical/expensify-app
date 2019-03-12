e0e1549b22bdf7b308c81567e6772b75fcad2c6a
Ian KIM <magictical@gmail.com>
2019-03-12 23:35:36 +0900

    change import lib from ExpenseForm to app.js for a moment

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 //import react-redux provider
 import { Provider } from 'react-redux';
@@ -9,18 +9,22 @@ import configureStore from './store/configureStore';
 //webpack에서 scss 설정을 css에서 적용되도록 중간에 '?' 추가 해줘야 함
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
-
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-const expenseThree = store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-
+const expenseOne = store.dispatch(
+  addExpense({ description: 'Water bill', amount: 4500 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: 'Gas bill', createdAt: 1000 })
+);
+const expenseThree = store.dispatch(
+  addExpense({ description: 'Rent', amount: 109500 })
+);
 
 // setTimeout(() => {
 //     store.dispatch(setTextFilter('bill'));
@@ -30,14 +34,10 @@ const state = store.getState();
 const showCurrentState = getVisibleExpenses(state.expenses, state.filters);
 console.log('from app.js', showCurrentState);
 
-
-
-
-
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 
 const now = moment();
 
@@ -10,93 +9,92 @@ console.log(now.format('MMM Do, YYYY'));
 export default class ExpenseForm extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       //local state for tracking the data in this page
-      description: props.expense ? props.expense.description :'',
-      note: props.expense ? props.expense.note :'',
-      amount: props.expense ? (props.expense.amount / 100).toString():  '',
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
       createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
       calendarFocused: false,
       error: ''
     };
-  } 
-  
+  }
+
   // tracking the input data from user in description <input>
-  onDescriptionChange = (e) => {
+  onDescriptionChange = e => {
     const description = e.target.value;
     this.setState(() => ({ description }));
-  }
-  onNoteChange = (e) => {
-    const note = e.target.value; 
+  };
+  onNoteChange = e => {
+    const note = e.target.value;
     this.setState(() => ({ note }));
-  }
-  onAmountChange = (e) => {
+  };
+  onAmountChange = e => {
     const amount = e.target.value;
     //regex for number and decimal two point
     //!amount 는 input값을 지웠을경우(false) input이 placeholder로 초기화되도록 함
-    if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
-      this.setState(() => ({ amount }))
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }));
     }
-  }
-  onDateChange = (createdAt) => {
+  };
+  onDateChange = createdAt => {
     //prevent delete the date(colored UI) from user
-    if(createdAt) {
+    if (createdAt) {
       this.setState(() => ({ createdAt }));
-    }    
-  }
+    }
+  };
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
-  }
-  onSubmit = (e) => {
+  };
+  onSubmit = e => {
     e.preventDefault();
-    if(!this.state.description || !this.state.amount) {
+    if (!this.state.description || !this.state.amount) {
       this.setState(() => ({ error: 'Please provide description and amount' }));
     } else {
-      this.setState(() => ({ error: ''}));
+      this.setState(() => ({ error: '' }));
       this.props.onSubmit({
-        description:this.state.description,
+        description: this.state.description,
         amount: parseFloat(this.state.amount, 10) * 100,
         createdAt: this.state.createdAt.valueOf(),
         note: this.state.note
       });
     }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="text"
+            placeholder="Amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+          <textarea
+            placeholder="Add a note for your expense(optional)"
+            onChange={this.onNoteChange}
+          />
+          <button>Add Expense</button>
+        </form>
+      </div>
+    );
   }
-
-
-    render() {
-        return (
-            <div>
-            {this.state.error && <p>{this.state.error}</p>  }
-                <form onSubmit={this.onSubmit}>
-                  <input 
-                    type="text"
-                    placeholder="Description"
-                    autoFocus
-                    value={this.state.description}
-                    onChange={ this.onDescriptionChange }
-                  />
-                  <input
-                    type="text"
-                    placeholder="Amount"
-                    value={this.state.amount}
-                    onChange={ this.onAmountChange }
-                  />
-                  <SingleDatePicker 
-                    date={this.state.createdAt}
-                    onDateChange={this.onDateChange}
-                    focused={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                  />
-                  <textarea
-                    placeholder="Add a note for your expense(optional)"
-                    onChange={this.onNoteChange}
-                  />
-                  <button>Add Expense</button>
-                </form>
-            </div>
-        )
-    }
 }

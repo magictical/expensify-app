c496155d417e7b5f568a5f24fb0c8ab99f48b209
Ian KIM <magictical@gmail.com>
2019-04-12 10:35:51 +0900

    lec124 - Testing AddExpensePage
    change to class base component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,20 +3,27 @@ import ExpenseForm from './ExpenseForm';
 import { connect } from 'react-redux';
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-				<ExpenseForm 
-					//expense 빈박스를 보내서 ExpenseFrom에서 가공한후
-					//dispatch로 store와 연결하면 이제 다른 component에도 사용가능!
-          onSubmit={(expense) => {
-						props.dispatch(addExpense(expense));
-						//move to dashboard
-						props.history.push('/');
-						
-					}}
-        />
-    </div>
-);
+export class AddExpensePage extends React.Component {
+  onSubmit = expense => {
+    this.props.onSubmit(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1> Add Expense </h1>
+        <ExpenseForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
 
-export default connect()(AddExpensePage);
+const mapDispatchToProps = dispatch => ({
+  onSubmit: expense => {
+    dispatch(addExpense(expense));
+  }
+});
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(AddExpensePage);

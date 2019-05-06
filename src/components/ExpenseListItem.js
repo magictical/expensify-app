7b7ed7951f9f5483c72804f0a0321f657723637c
Ian KIM <magictical@gmail.com>
2019-05-06 16:09:40 +0900

    lec138 - import moment.js to 'createdAt'

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 // Export a stateless fuctional component
-// destructuring으로 props을 - Descruption, amount, createdAt 으로 
+// destructuring으로 props을 - Descruption, amount, createdAt 으로
 // 지정해서 받아서 사용!
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-    <div>
-        <Link to= {`/edit/${id}`}>
-           <h3>{description}</h3>
-        </Link>
-        <p>{amount} - {createdAt}</p>
-    </div>
-)
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {amount} - {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+  </div>
+);
 
 export default ExpenseListItem;
